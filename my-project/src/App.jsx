06d3ba3646d9393077a111ca540ed255614dcb44
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 10:09:31 -0800

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
